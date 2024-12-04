85b886c6208de51e9cab4557f2875c9efdc32fe7
Robert <robert.pooenaru@gmail.com>
2024-12-04 17:18:56 +0100

    Banco online

---
+++ Banco online/Client.cs
@@ -0,0 +1,19 @@
+namespace Banquer;
+
+public class Client
+{
+    public Client(string nomClient)
+    {
+        Nom = nomClient;
+        Saldo = 0;
+    }
+    public string Nom { get; }
+
+    // private saldo;
+    // public getSaldo() { return saldo; }
+    // public setSaldo(int x) { saldo = x; }
+
+    public int Saldo { get; set; } = 0;
+
+
+}
